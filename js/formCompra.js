2cfb67c35437f91e048731490d7c030cdbddc5d7
arual_zeravla <laura.q.alvarez@gmail.com>
2022-07-29 00:25:33 -0300

    carrito2

--- js/formCompra.js
+++ js/formCompra.js
@@ -1,32 +1,45 @@
 //DEFINO PRODUCTOS EN STOCK
 
 let productos = [{
-    id: 1,
-    titulo: "Cómo era ser pequeño",
-    precio: 1899,
-    idBoton: "carrito_serPequeno"
-},
-{
-    id: 2,
-    titulo: "El arte de la guerra",
-    precio: 1499,
-    idBoton: "carrito_guerra"
-},
-{
-    id: 3,
-    titulo: "Boquita",
-    precio: 2199,
-    idBoton: "carrito_boquita"
-}
+        id: 1,
+        titulo: "Cómo era ser pequeño",
+        precio: 1899,
+        idBoton: "carrito_serPequeno"
+    },
+    {
+        id: 2,
+        titulo: "El arte de la guerra",
+        precio: 1499,
+        idBoton: "carrito_guerra"
+    },
+    {
+        id: 3,
+        titulo: "Boquita",
+        precio: 2199,
+        idBoton: "carrito_boquita"
+    }
 ]
 
 
 
 //TOMO EL CARRITO DEL LOCAL STORAGE Y LO INSERTO EN EL HTML
 
-let precioTotal = 0;
 let localCarrito = localStorage.getItem("carrito");
 let carrito = JSON.parse(localCarrito);
+
+
+function actualizarPrecio() {
+    let precio = 0;
+    for (producto of carrito) {
+        precio = precio + producto.precio;
+
+        precioFinal = precio.toFixed(2);
+        
+        let totalDiv = document.getElementById("div_sumaPrecio");
+        totalDiv.innerHTML = ` $ ${precioFinal} `;
+    }
+};
+
 
 function renderizarCarrito() {
     for (item of carrito) {
@@ -36,32 +49,42 @@ function renderizarCarrito() {
                                 <div class="div_tituloCarrito">${item.titulo}</div>
                                 <div class="div_precioCarrito"> $ ${precioFormatted}</div>
                                 <div class="div_trashIcon"> <i class="fa-solid fa-trash-can"></i> </div>
-                            </li>`
+                            </li>`  
+                        };
+    actualizarPrecio();
 
-        precioTotal = precioTotal + item.precio;
-        precioTotalForm = precioTotal.toFixed(2);
-        let totalDiv = document.getElementById("div_sumaPrecio");
-        totalDiv.innerHTML = ` $ ${precioTotalForm} `;
-
-    };
 };
 
 renderizarCarrito();
 
-//BORRO ITEM DEL HTML AL HACER CLICK EN EL ICONO DE BORRAR
 
+
+
+//BORRO ITEM DEL LOCAL STORAGE Y DEL HTML AL HACER CLICK EN EL ICONO DE BORRAR
+
+//ATRAPO TODOS LOS LI DEL HTML QUE REPRESENTA EL CARRITO Y LE AGREGO UN EVENT LISTENER A CADA UNO DE SUS ICONOS DE BORRAR:
 let itemsCarrito = document.querySelectorAll("li.li_itemCarrito");
+
 for (item of itemsCarrito) {
     let trashIcon = item.children[2];
-    let btnParent = trashIcon.parentNode;
-    let divTitle = btnParent.children[0].innerHTML;
     trashIcon.addEventListener("click", () => {
-        let itemDiv = trashIcon.parentElement;
+        //ATRAPO EL DIV COMPLETO DE LA LINEA A BORRAR:
+        let itemDiv = trashIcon.parentNode;
+
+        //GENERO UN ARRAY DE LOS TITULOS DEL CARRITO QUE ESTA EN EL LS Y PIDO EL INDEX QUE TENGA EL TITULO IGUAL AL DIV QUE ESTOY BORRANDO. LUEGO LO ELIMINO DEL ARRAY CARRITO:
+        let index = carrito.map(producto => producto.titulo).indexOf(itemDiv.children[0].innerHTML.toString());
+        carrito.splice(index, 1);
+
+        //ATRAPO EL NUEVO CARRITO Y LO ENVÍO AL LS:
+        let nuevoCarrito = JSON.stringify(carrito);
+        localStorage.setItem("carrito", nuevoCarrito);
+
+        //BORRO EL DIV DEL HTML:
         itemDiv.innerHTML = "";
-        
-        
-        localStorage.getItem("carrito");
-        
+
+        actualizarPrecio();
+
+
     })
 };
 
